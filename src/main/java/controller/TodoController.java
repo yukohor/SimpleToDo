6c800497d2f5166f2626a17061ff039b1b6ef0ed
username <170209740+yukohor@users.noreply.github.com>
2025-01-27 22:01:42 +0900

    1/27　大幅変更

---
+++ src/main/java/controller/TodoController.java
@@ -0,0 +1,18 @@
+package controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("todo")
+public class TodoController {
+
+
+/*}
+
+@GetmMapping("todos")
+public String getList(Model model) {
+	model.addAttribute("todos",service.todos());
+	return "todos/";
+
+*/
+}
